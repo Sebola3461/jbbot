a1f683f2a092e0c85423ecd7ff969891d1077dfc
Paulo Ricardo <62405013+Sebola3461@users.noreply.github.com>
2026-04-05 18:19:44 -0300

    Update Calculadora.java

--- Calculadora.java
+++ Calculadora.java
@@ -15,6 +15,7 @@ public class Calculadora {
     }
 
     // Método principal para testar
+    // bingas
     public static void main(String[] args) {
         Calculadora calc = new Calculadora();
 
